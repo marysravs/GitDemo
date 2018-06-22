f845a9c78b920104d97599929c0f32c9f8da2744
sravs <mary_basani@yahoo.com>
2018-06-21 21:29:28 -0400

    framework guy first change

--- src/test/java/qaclickacademy/AppiumTest.java
+++ src/test/java/qaclickacademy/AppiumTest.java
@@ -6,6 +6,8 @@ public class AppiumTest {
 	@Test
 	public void NativeappAndroid() {
 		System.out.println("Android app working");
+		System.out.println("selenium branch");
+		System.out.println("branch to be committed");
 	}
 @Test
 public void IOSapp() {
--- src/test/java/qaclickacademy/SeleniumTest.java
+++ src/test/java/qaclickacademy/SeleniumTest.java
@@ -6,9 +6,11 @@ public class SeleniumTest {
 	@Test
 	public void browserAutomation() {
 		System.out.println("google.com");
+		System.out.println("second branch change");
 	}
 	@Test
 public void elementsUI() {
-		System.out.println("radio and check buttons");
+		System.out.println("radio and check buttons"); 
+		 System.out.println("jgoep");
 	}
 }
