36d2667f13642b726e88bfc25e27754a1cbbda12
sravs <mary_basani@yahoo.com>
2018-06-21 20:24:43 -0400

    this is the first change done

--- src/test/java/qaclickacademy/RestApiTest.java
+++ src/test/java/qaclickacademy/RestApiTest.java
@@ -6,6 +6,8 @@ public class RestApiTest {
 	@Test
 	public void postJira() {
 		System.out.println("postJira");
+		System.out.println("this is the first line change");
+		System.out.println("this is the second line change");
 	}
 	@Test
 	public void delTwitter() {
